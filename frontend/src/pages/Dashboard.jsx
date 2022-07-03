b1e31b6001866677e6fcf07f54e592e6af2b7114
jjuiyuc <action5392@gmail.com>
2022-07-03 15:09:37 +0800

    dashboard/palette/variable/tailwind.config

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,38 @@
+import {useState}from "react"
+import {useTranslation} from "react-multi-lang"
+import variables from "../configs/variables"
+
+
+
 export default function Dashboard () {
-    return <h1>Dashboard</h1>
+    const
+    t = useTranslation(),
+    commonT = string => t("common." + string)
+
+    const
+    [current,setCurrent] = useState(13),
+    [threshhold,setThreshhold] = useState(15)
+
+    return <>
+    <h1>Dashboard</h1>
+    <div className="card">
+        <div className="flex flex-wrap items-baseline mb-6">
+            <h5 className="font-bold">Peak Shave</h5>
+        </div>
+        <div className="flex h-2 overflow-hidden rounded-full w-full bg-gray-600">
+            <div
+                className="bg-positive-main h-full"
+                style={{ width:'40%' }}
+            />
+        </div>
+        <div className="text-28px font-bold">
+            <span className={current > threshhold ? "text-negative-main" : "text-positive-main"}>
+                {current} 
+            </span>/ {threshhold} {commonT("kw")}
+        </div>
+    </div>
+       
+    </>
+    
+
 }

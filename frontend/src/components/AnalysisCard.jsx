da0ba0bdc06493299b51d82a6b05166d3d65f7be
jjuiyuc <action5392@gmail.com>
2022-07-13 14:13:39 +0800

    analysis imformation cards

---
+++ frontend/src/components/AnalysisCard.jsx
@@ -0,0 +1,52 @@
+import { useTranslation } from "react-multi-lang"
+
+const colors = {
+    directSolarSupply: "bg-yellow-main",
+    importFromGrid: "bg-indigo-main",
+    batteryDischarge: "bg-blue-main",
+    load: "bg-green-main",
+    exportFromGrid: "bg-indigo-main",
+    chargeToBattery: "bg-blue-main"
+}
+
+export default function AnalysisCard(props) {
+    const
+        t = useTranslation(),
+        commonT = string => t("common." + string),
+        pageT = (string, params) => t("analysis." + string, params)
+
+    const { kwh, types } = props.data
+
+    return <div className="card">
+        <div className="flex flex-wrap items-baseline mb-6">
+            <h2 className="mr-2 whitespace-nowrap">{kwh} {commonT("kwh")}</h2>
+            <h5 className="font-bold">{props.title}</h5>
+        </div>
+        <div className="flex h-2 overflow-hidden rounded-full w-full">
+            {types.map((t, i) =>
+                <div
+                    className={colors[t.type] + " h-full"}
+                    key={"bar-" + i}
+                    style={{ width: t.percentage + "%" }} />)}
+        </div>
+        <div className="flex justify-center mb-12 mt-4">
+            {types.map((t, i) =>
+                <div
+                    className="flex items-center mx-2.5 text-white text-xs"
+                    key={"legend-" + i}>
+                    <div className={colors[t.type] + " h-3 mr-2 rounded-full w-3"} />
+                    {pageT(t.type)}
+                </div>)}
+        </div>
+        <div className="grid grid-cols-3 three-columns gap-x-5 sm:gap-x-10">
+            {types.map((t, i) =>
+                <div key={"detail-" + i}>
+                    <h6 className="font-bold text-white">{pageT(t.type)}</h6>
+                    <h3 className="my-1">{t.percentage}%</h3>
+                    <p className="lg:test text-13px text-white">
+                        {t.kwh} {commonT("kwh")}
+                    </p>
+                </div>)}
+        </div>
+    </div>
+}
---
+++ frontend/src/pages/Analysis.jsx
@@ -0,0 +1,118 @@
+import { Button, Stack, TextField, Box } from "@mui/material"
+import { DateRangePickerDay } from '@mui/x-date-pickers-pro'
+import { Fragment as Frag, useEffect, useRef, useState } from "react"
+import moment from "moment"
+import { useTranslation } from "react-multi-lang"
+
+import AnalysisCard from "../components/AnalysisCard"
+import variables from "../configs/variables"
+
+const { colors } = variables
+
+export default function Analysis() {
+
+    const
+        t = useTranslation(),
+        commonT = string => t("common." + string),
+        errorT = string => t("error." + string),
+        formT = string => t("form." + string),
+        pageT = (string, params) => t("analysis." + string, params)
+
+    const analysisCardTitle = () => {
+        <>
+            <span className="inline-block mr-1">
+                {pageT("totalEnergySources")}
+            </span>
+            <span className="inline-block mr-1">
+                {pageT("energyDestinations")}
+            </span>
+
+        </>
+    }
+    const [value, setValue] = useState([null, null])
+    const
+        hours24 = Array.from(new Array(24).keys()),
+        lineChartDateLabels = hours24.map(n => {
+            const time = moment().hour(n).minute(0).second(0)
+
+            return time.format("hh A")
+        }),
+        currentHour = moment().hour(),
+        lineChartDataArray = hours24.filter(v => v <= currentHour).map(() =>
+            Math.floor(Math.random() * (60 - 40 + 1) + 40))
+
+    const
+        [totalEnergySources, setTotalEnergySources] = useState({
+            types: [
+                { kwh: 7.5, percentage: 15, type: "directSolarSupply" },
+                { kwh: 30, percentage: 60, type: "importFromGrid" },
+                { kwh: 12.5, percentage: 25, type: "batteryDischarge" },
+            ],
+            kwh: 50
+        }),
+
+        [energyDestinations, setEnergyDestinations] = useState({
+            types: [
+                { kwh: 10, percentage: 18, type: "load" },
+                { kwh: 25, percentage: 41, type: "exportFromGrid" },
+                { kwh: 25, percentage: 41, type: "batteryDischarge" },
+            ],
+            kwh: 60
+        }),
+        [prices, setPrices]
+            = useState({ onPeak: 0, midPeak: 0, offPeak: 0, superOffPeak: 0 }),
+
+        [tab, setTab] = useState("days")
+
+    return <>
+        <div className="page-header">
+            <h1>{pageT("analysis")}</h1>
+            <Stack direction="row" justifyContent="flex-end" spacing={1.5}>
+                <Button
+                    onClick={() => setTab("days")}
+                    filter={tab === "days" ? "selected" : ""}
+                    radius="pill"
+                    variant="contained">
+                    {pageT("days")}
+                </Button>
+                <Button
+                    onClick={() => setTab("weeks")}
+                    filter={tab === "weeks" ? "selected" : ""}
+                    radius="pill"
+                    variant="contained">
+                    {pageT("weeks")}
+                </Button>
+                <Button
+                    onClick={() => setTab("month")}
+                    filter={tab === "month" ? "selected" : ""}
+                    radius="pill"
+                    variant="contained">
+                    {pageT("month")}
+                </Button>
+                <Button
+                    onClick={() => setTab("year")}
+                    filter={tab === "year" ? "selected" : ""}
+                    radius="pill"
+                    variant="contained">
+                    {pageT("year")}
+                </Button>
+                <Button
+                    onClick={() => setTab("custom")}
+                    filter={tab === "custom" ? "selected" : ""}
+                    radius="pill"
+                    variant="contained">
+                    {pageT("custom")}
+                </Button>
+                {/* {tab === "custom"
+                    ? <>
+                        <DateRangePickerDay />
+                    </>
+                    : null} */}
+            </Stack>
+        </div>
+        <div className="gap-8 grid md:grid-cols-2 items-start">
+            <AnalysisCard data={totalEnergySources} title={analysisCardTitle("totalEnergySources")} />
+            <AnalysisCard data={energyDestinations} title={analysisCardTitle("energyDestinations")} />
+        </div>
+    </>
+}

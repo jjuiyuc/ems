a25759d17d88c94e2ac030277f3189e6adee5666
jjuiyuc <action5392@gmail.com>
2022-07-19 17:13:40 +0800

    feature / dashboard: websocket connection

--- frontend/src/constant/env.js
+++ frontend/src/constant/env.js
@@ -1,1 +1,6 @@
-export const API_HOST = import.meta.env.VITE_APP_API_ENDPOINT;
+const
+    envPort = import.meta.env.VITE_APP_API_PORT,
+    windowPort = window.location.port,
+    port = (envPort || windowPort) ? (":" + (envPort || windowPort)) : ""
+
+export const API_HOST = import.meta.env.VITE_APP_API_ENDPOINT + port

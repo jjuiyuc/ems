54845bd001c16eeadebefa7dffb26bdab6fcb274
jjuiyuc <action5392@gmail.com>
2022-08-02 10:54:20 +0800

    0802

--- frontend/src/components/PriceCard.jsx
+++ frontend/src/components/PriceCard.jsx
@@ -4,8 +4,8 @@ export default function PriceCard(props) {
 
     return <div className="card mr-5 md:mb-8">
         <div>
-            <p>{props.title}</p>
-            <h2 className="font-bold mb-1">${price}</h2>
+            <p className="font-bold">{props.title}</p>
+            <h2 className="font-bold mb-1 pt-4">${price}</h2>
         </div>
     </div>
 }

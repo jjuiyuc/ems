c55f5abe468d885554d5a315b935b900e03b9b4e
jjuiyuc <action5392@gmail.com>
2022-09-14 17:58:01 +0800

    feat: enable get api /{gwid}/devices/energy-distribution-info

--- backend/services/devices.go
+++ backend/services/devices.go
@@ -34,9 +34,28 @@ type DevicesEnergyInfoResponse struct {
 	GridConsumedAveragePowerAC    float32                `json:"gridConsumedAveragePowerAC"`
 }
 
+// EnergyDistributionInfoResponse godoc
+type EnergyDistributionInfoResponse struct {
+	AllProducedLifetimeEnergyACDiff     float32 `json:"allProducedLifetimeEnergyACDiff"`
+	PvProducedEnergyPercentAC           float32 `json:"pvProducedEnergyPercentAC"`
+	GridProducedEnergyPercentAC         float32 `json:"gridProducedEnergyPercentAC"`
+	BatteryProducedEnergyPercentAC      float32 `json:"batteryProducedEnergyPercentAC"`
+	PvProducedLifetimeEnergyACDiff      float32 `json:"pvProducedLifetimeEnergyACDiff"`
+	GridProducedLifetimeEnergyACDiff    float32 `json:"gridProducedLifetimeEnergyACDiff"`
+	BatteryProducedLifetimeEnergyACDiff float32 `json:"batteryProducedLifetimeEnergyACDiff"`
+	AllConsumedLifetimeEnergyACDiff     float32 `json:"allConsumedLifetimeEnergyACDiff"`
+	LoadConsumedEnergyPercentAC         float32 `json:"loadConsumedEnergyPercentAC"`
+	GridConsumedEnergyPercentAC         float32 `json:"gridConsumedEnergyPercentAC"`
+	BatteryConsumedEnergyPercentAC      float32 `json:"batteryConsumedEnergyPercentAC"`
+	LoadConsumedLifetimeEnergyACDiff    float32 `json:"loadConsumedLifetimeEnergyACDiff"`
+	GridConsumedLifetimeEnergyACDiff    float32 `json:"gridConsumedLifetimeEnergyACDiff"`
+	BatteryConsumedLifetimeEnergyACDiff float32 `json:"batteryConsumedLifetimeEnergyACDiff"`
+}
+
 // DevicesService godoc
 type DevicesService interface {
 	GetLatestDevicesEnergyInfo(gwUUID string) (updatedTime time.Time, devicesEnergyInfo *DevicesEnergyInfoResponse, err error)
+	GetEnergyDistributionInfo(gwUUID string, startTime, endTime time.Time) (energyDistributionInfo *EnergyDistributionInfoResponse)
 }
 
 type defaultDevicesService struct {
@@ -106,6 +125,56 @@ func (s defaultDevicesService) GetLatestDevicesEnergyInfo(gwUUID string) (logTim
 			"caused-by": "json.Unmarshal: batteryLinksValue",
 			"err":       err,
 		}).Error()
+	}
+	return
+}
+
+func (s defaultDevicesService) GetEnergyDistributionInfo(gwUUID string, startTime, endTime time.Time) (energyDistributionInfo *EnergyDistributionInfoResponse) {
+	energyDistributionInfo = &EnergyDistributionInfoResponse{}
+	firstlog, err1 := s.repo.CCData.GetFirstLogByGatewayUUIDAndPeriod(gwUUID, startTime, endTime)
+	latestLog, err2 := s.repo.CCData.GetLatestLogByGatewayUUIDAndPeriod(gwUUID, startTime, endTime)
+
+	if err1 != nil || err2 != nil {
+		log.WithFields(log.Fields{
+			"caused-by": "s.repo.CCData.GetFirstLogByGatewayUUIDAndPeriod and GetLatestLogByGatewayUUIDAndPeriod",
+			"err1":      err1,
+			"err2":      err2,
+		}).Error()
+		return
+	}
+
+	log.Debug("firstlog.LogDate: ", firstlog.LogDate)
+	log.Debug("latestLog.LogDate: ", latestLog.LogDate)
+	if firstlog.LogDate == latestLog.LogDate {
+		log.WithFields(log.Fields{"caused-by": "firstlog.LogDate == latestLog.LogDate"}).Warn()
+		return
+	}
+
+	energyDistributionInfo.AllProducedLifetimeEnergyACDiff = latestLog.AllProducedLifetimeEnergyAC.Float32 - firstlog.AllProducedLifetimeEnergyAC.Float32
+	energyDistributionInfo.PvProducedLifetimeEnergyACDiff = latestLog.PvProducedLifetimeEnergyAC.Float32 - firstlog.PvProducedLifetimeEnergyAC.Float32
+	energyDistributionInfo.GridProducedLifetimeEnergyACDiff = latestLog.GridProducedLifetimeEnergyAC.Float32 - firstlog.GridProducedLifetimeEnergyAC.Float32
+	energyDistributionInfo.BatteryProducedLifetimeEnergyACDiff = latestLog.BatteryProducedLifetimeEnergyAC.Float32 - firstlog.BatteryProducedLifetimeEnergyAC.Float32
+	if energyDistributionInfo.AllProducedLifetimeEnergyACDiff == 0 {
+		energyDistributionInfo.PvProducedEnergyPercentAC = 0
+		energyDistributionInfo.GridProducedEnergyPercentAC = 0
+		energyDistributionInfo.BatteryProducedEnergyPercentAC = 0
+	} else {
+		energyDistributionInfo.PvProducedEnergyPercentAC = (energyDistributionInfo.PvProducedLifetimeEnergyACDiff / energyDistributionInfo.AllProducedLifetimeEnergyACDiff) * 100
+		energyDistributionInfo.GridProducedEnergyPercentAC = (energyDistributionInfo.GridProducedLifetimeEnergyACDiff / energyDistributionInfo.AllProducedLifetimeEnergyACDiff) * 100
+		energyDistributionInfo.BatteryProducedEnergyPercentAC = (energyDistributionInfo.BatteryProducedLifetimeEnergyACDiff / energyDistributionInfo.AllProducedLifetimeEnergyACDiff) * 100
+	}
+	energyDistributionInfo.AllConsumedLifetimeEnergyACDiff = latestLog.AllConsumedLifetimeEnergyAC.Float32 - firstlog.AllConsumedLifetimeEnergyAC.Float32
+	energyDistributionInfo.LoadConsumedLifetimeEnergyACDiff = latestLog.LoadConsumedLifetimeEnergyAC.Float32 - firstlog.LoadConsumedLifetimeEnergyAC.Float32
+	energyDistributionInfo.GridConsumedLifetimeEnergyACDiff = latestLog.GridConsumedLifetimeEnergyAC.Float32 - firstlog.GridConsumedLifetimeEnergyAC.Float32
+	energyDistributionInfo.BatteryConsumedLifetimeEnergyACDiff = latestLog.BatteryConsumedLifetimeEnergyAC.Float32 - firstlog.BatteryConsumedLifetimeEnergyAC.Float32
+	if energyDistributionInfo.AllConsumedLifetimeEnergyACDiff == 0 {
+		energyDistributionInfo.LoadConsumedEnergyPercentAC = 0
+		energyDistributionInfo.GridConsumedEnergyPercentAC = 0
+		energyDistributionInfo.BatteryConsumedEnergyPercentAC = 0
+	} else {
+		energyDistributionInfo.LoadConsumedEnergyPercentAC = (energyDistributionInfo.LoadConsumedLifetimeEnergyACDiff / energyDistributionInfo.AllConsumedLifetimeEnergyACDiff) * 100
+		energyDistributionInfo.GridConsumedEnergyPercentAC = (energyDistributionInfo.GridConsumedLifetimeEnergyACDiff / energyDistributionInfo.AllConsumedLifetimeEnergyACDiff) * 100
+		energyDistributionInfo.BatteryConsumedEnergyPercentAC = (energyDistributionInfo.BatteryConsumedLifetimeEnergyACDiff / energyDistributionInfo.AllConsumedLifetimeEnergyACDiff) * 100
 	}
 	return
 }

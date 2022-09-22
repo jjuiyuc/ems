2144b45ba0ef9c9fd5480d1bd4a9f9fd32a47d98
jjuiyuc <action5392@gmail.com>
2022-09-22 15:23:33 +0800

    docs: update swagger docs

--- backend/routers/demand-charge.go
+++ backend/routers/demand-charge.go
@@ -22,6 +22,18 @@ func (w *APIWorker) GetChargeInfo(c *gin.Context) {
 }
 
 // GetDemandState godoc
+// @Summary     Show today's demand details
+// @Description get demand details by token, gateway UUID, startTime and endTime
+// @Tags        demand charge
+// @Security    ApiKeyAuth
+// @Param       Authorization  header    string true "Input user's access token" default(Bearer <Add access token here>)
+// @Param       gwid           path      string true "Gateway UUID"
+// @Param       query          query     PeriodQuery true "Query"
+// @Produce     json
+// @Success     200            {object}  app.Response{data=services.DemandStateResponse}
+// @Failure     400            {object}  app.Response
+// @Failure     401            {object}  app.Response
+// @Router      /{gwid}/devices/demand-state [get]
 func (w *APIWorker) GetDemandState(c *gin.Context) {
 	w.getPeriodInfo(c, DemandState)
 }

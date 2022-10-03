fd58cfe2e7e853637b79fcabf4ac0582a54fbe23
jjuiyuc <action5392@gmail.com>
2022-10-03 13:46:50 +0800

    fix: wrong gridLifetimeEnergyACDiffToPowers value in get api /{gwid}/devices/demand-state

--- backend/internal/utils/number.go
+++ backend/internal/utils/number.go
@@ -20,6 +20,23 @@ func Diff(x, y float32) float32 {
 	return float32(value)
 }
 
+// Division godoc
+func Division(x, y float32) float32 {
+	if x == 0 || y == 0 {
+		log.WithFields(log.Fields{"caused-by": "numerator/denominator is zero"}).Error()
+		return 0
+	}
+	value, err := strconv.ParseFloat(fmt.Sprintf("%.2f", x/y), 32)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"caused-by": "division parse",
+			"err":       err,
+		}).Error()
+		return 0
+	}
+	return float32(value)
+}
+
 // Percent godoc
 func Percent(x, y float32) float32 {
 	if x == 0 || y == 0 {
--- backend/services/devices.go
+++ backend/services/devices.go
@@ -693,9 +693,9 @@ func (s defaultDevicesService) getLatestComputedDemandState(gwUUID string, start
 	}
 
 	latestComputedDemandState.Timestamps = int(latestLog.LogDate.Unix())
-	latestComputedDemandState.GridLifetimeEnergyACDiffToPower = utils.Percent(
+	latestComputedDemandState.GridLifetimeEnergyACDiffToPower = utils.Division(
 		utils.Diff(latestLog.GridLifetimeEnergyAC.Float32,
-			firstLog.GridLifetimeEnergyAC.Float32), (15 / 60))
+			firstLog.GridLifetimeEnergyAC.Float32), (15.0 / 60.0))
 	latestComputedDemandState.GridContractPowerAC = latestLog.GridContractPowerAC.Float32
 	return
 }

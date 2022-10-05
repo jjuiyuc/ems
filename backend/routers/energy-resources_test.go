72c4d9b3c085a9c743e1f93cc554b8f69ce616cd
jjuiyuc <action5392@gmail.com>
2022-10-05 17:03:33 +0800

    style: typo

--- backend/routers/energy-resources_test.go
+++ backend/routers/energy-resources_test.go
@@ -20,7 +20,7 @@ import (
 	"der-ems/testutils/fixtures"
 )
 
-var _ = Describe("DemandCharge", func() {
+var _ = Describe("EnergyResources", func() {
 	const (
 		UtStartTime = "2022-08-03T16:00:00.000Z"
 		UtEndTime   = "2022-08-03T20:15:00.000Z"

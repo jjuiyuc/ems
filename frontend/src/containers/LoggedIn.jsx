2d6ba2113f0ed55c9c5b79d8d3e70b3ef2ed12db
jjuiyuc <action5392@gmail.com>
2022-10-13 11:33:01 +0800

    feat : hiding Settings, Economics

--- frontend/src/containers/LoggedIn.jsx
+++ frontend/src/containers/LoggedIn.jsx
@@ -45,7 +45,7 @@ function LoggedIn(props) {
                         <Route element={<Dashboard />} path="/dashboard" />
                         <Route element={<Analysis />} path="/analysis" />
                         <Route element={<TimeOfUse />} path="/time-of-use" />
-                        <Route element={<Economics />} path="/economics" />
+                        {/* <Route element={<Economics />} path="/economics" /> */}
                         <Route
                             element={<DemandCharge />}
                             path="/demand-charge" />
@@ -66,7 +66,7 @@ function LoggedIn(props) {
                         <Route
                             element={<Navigate to="/dashboard" replace />}
                             path="*" />
-                        <Route element={<Settings />} path="/settings" />
+                        {/* <Route element={<Settings />} path="/settings" /> */}
                     </Routes>
                 </div>
             </div>

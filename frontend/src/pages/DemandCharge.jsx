14871126bdc128fc8941650ea67301b30256048e
jjuiyuc <action5392@gmail.com>
2022-10-19 16:21:46 +0800

    style: add missing space in DemandCharge chart

--- frontend/src/pages/DemandCharge.jsx
+++ frontend/src/pages/DemandCharge.jsx
@@ -60,7 +60,7 @@ export default connect(mapState)(function DemandCharge(props) {
         }],
         labels,
         tickCallback: (val, index) => val + commonT("kw"),
-        tooltipLabel: item => `${item.parsed.y}` + commonT("kw"),
+        tooltipLabel: item => `${item.parsed.y}` + " " + commonT("kw"),
         x: { grid: { lineWidth: 0 } },
         y: { max: 80, min: 0 },
         beforeDraw: chart => {

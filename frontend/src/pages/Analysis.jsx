39f0a3ecf0c4480cd10f6dbcb87aa0c374da2a86
jjuiyuc <action5392@gmail.com>
2022-11-01 11:03:01 +0800

    Fix Analysis MONTH & YEAR tab api endTime parameter are wrong (startTime and endTime is the same)

--- frontend/src/pages/Analysis.jsx
+++ frontend/src/pages/Analysis.jsx
@@ -23,7 +23,7 @@ const ErrorBox = ({ error, margin = "", message }) => error
             <span className="font-mono ml-2">{error}</span>
             <span className="ml-2">{message}</span>
         </>}
-        icon={ReportProblemIcon}
+        // icon={ReportProblemIcon}
         iconColor="negative-main" />
     : null
 const LoadingBox = ({ loading }) => loading
@@ -221,7 +221,7 @@ export default connect(mapState)(function Analysis(props) {
         [lineChartPower, setLineChartPower] = useState(null),
         [lineChartPowerError, setLineChartPowerError] = useState(""),
         [lineChartPowerLoading, setLineChartPowerLoading] = useState(false),
-        [lineChartPowertRes] = useState("5minute"),
+        [lineChartPowerRes] = useState("5minute"),
         [barChartData, setBarChartData] = useState(null),
         [barChartDataError, setBarChartDataError] = useState(""),
         [barChartDataLoading, setBarChartDataLoading] = useState(false),
@@ -289,7 +289,7 @@ export default connect(mapState)(function Analysis(props) {
         callLineChartPower = (startTime, endTime) => {
             const lineChartPowerUrl = `${urlPrefix}/power-state?`
                 + new URLSearchParams({
-                    startTime, endTime, resolution: lineChartPowertRes
+                    startTime, endTime, resolution: lineChartPowerRes
                 }).toString()
 
             apiCall({
@@ -388,14 +388,21 @@ export default connect(mapState)(function Analysis(props) {
         } else if (tab === "month") {
             startTime = moment().startOf("month").toISOString()
             endTime = moment().startOf("day").toISOString()
-
+            if (moment().get("date") == 1) {
+                startTime = moment().subtract(1, "month").startOf("month").toISOString()
+            }
         } else if (tab === "year") {
             startTime = moment().startOf("year").toISOString()
             endTime = moment().startOf("month").toISOString()
-
+            if (moment().get("date") == 1 && moment().get("month") == 0) {
+                startTime = moment().subtract(1, "year").startOf("year").toISOString()
+            }
         } else if (tab === "custom") {
             startTime = startDate ? moment(startDate).toISOString() : ""
             endTime = endDate ? moment(endDate).add(1, "day").startOf("day").toISOString() : ""
+        }
+        if (moment().get("day") == 0) {
+            startTime = moment().subtract(1, "week").startOf("week").toISOString()
         }
         if (startTime && endTime) {
             callCards(startTime, endTime)

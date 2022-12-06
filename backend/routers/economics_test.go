deab8669d4e4debe4a12374c0569da38fa3e2575
jjuiyuc <action5392@gmail.com>
2022-12-06 15:05:00 +0800

    test: add for get api /{gwid}/devices/tou/energy-cost

---
+++ backend/routers/economics_test.go
@@ -0,0 +1,134 @@
+package routers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"der-ems/config"
+	"der-ems/internal/app"
+	"der-ems/internal/e"
+	"der-ems/internal/utils"
+	"der-ems/models"
+	"der-ems/repository"
+	"der-ems/services"
+	"der-ems/testutils"
+	"der-ems/testutils/fixtures"
+)
+
+var _ = Describe("Economics", func() {
+	const (
+		UtStartTime = "2022-07-31T16:00:00.000Z"
+		UtEndTime   = "2022-08-02T16:00:00.000Z"
+	)
+
+	var (
+		router *gin.Engine
+		token  string
+		err    error
+	)
+
+	BeforeEach(func() {
+		config.Init(testutils.GetConfigDir(), "ut.yaml")
+		cfg := config.GetConfig()
+		models.Init(cfg)
+		db := models.GetDB()
+
+		repo := repository.NewRepository(db)
+		w := &APIWorker{
+			Services: services.NewServices(cfg, repo),
+		}
+
+		// Truncate & seed data
+		err = testutils.SeedUtUser(db)
+		Expect(err).Should(BeNil())
+		err = testutils.SeedUtCustomerAndGateway(db)
+		Expect(err).Should(BeNil())
+		token, err = utils.GenerateToken(fixtures.UtUser.ID)
+		Expect(err).Should(BeNil())
+		// Mock user_gateway_right table
+		_, err = db.Exec("TRUNCATE TABLE user_gateway_right")
+		Expect(err).Should(BeNil())
+		_, err = db.Exec(`
+			INSERT INTO user_gateway_right (id,user_id,gw_id) VALUES
+			(1,1,1);
+		`)
+		Expect(err).Should(BeNil())
+
+		router = InitRouter(cfg.GetBool("server.cors"), cfg.GetString("server.ginMode"), w)
+	})
+
+	AfterEach(func() {
+		models.Close()
+	})
+
+	Describe("GetTimeOfUseEnergyCost", func() {
+		Context("success", func() {
+			It("should be ok", func() {
+				prefixURL := fmt.Sprintf("/api/%s/devices/tou/energy-cost", fixtures.UtGateway.UUID)
+				seedUtURL := fmt.Sprintf("%s?startTime=%s&endTime=%s", prefixURL, UtStartTime, UtEndTime)
+				expectedResponseData := services.TimeOfUseEnergyCostResponse{
+					EnergyCosts: services.EnergyCostsInfo{
+						PreUbiikThisMonth:             798,
+						PostUbiikThisMonth:            636,
+						PreUbiikLastMonth:             0,
+						PostUbiikLastMonth:            0,
+						PreUbiikTheSameMonthLastYear:  0,
+						PostUbiikTheSameMonthLastYear: 0,
+					},
+					EnergyDailyCosts: services.EnergyCostsDailyInfo{
+						Timestamps:                    []int{1659369555, 1659455970},
+						PreUbiikThisMonth:             []int{411, 387},
+						PostUbiikThisMonth:            []int{340, 296},
+						PreUbiikLastMonth:             []int{0, 0},
+						PostUbiikLastMonth:            []int{0, 0},
+						PreUbiikTheSameMonthLastYear:  []int{0, 0},
+						PostUbiikTheSameMonthLastYear: []int{0, 0},
+						SavedThisMonth:                []int{71, 91},
+						SavedLastMonth:                []int{0, 0},
+						SavedTheSameMonthLastYear:     []int{0, 0},
+					},
+				}
+				tt := testutils.TestInfo{
+					Token:      token,
+					URL:        seedUtURL,
+					WantStatus: http.StatusOK,
+					WantRv: app.Response{
+						Code: e.Success,
+						Msg:  "ok",
+						Data: expectedResponseData,
+					},
+				}
+				rvData := testutils.GinkgoAssertRequest(tt, router, "GET", nil)
+				dataMap := rvData.(map[string]interface{})
+				dataJSON, err := json.Marshal(dataMap)
+				Expect(err).Should(BeNil())
+				var data services.TimeOfUseEnergyCostResponse
+				err = json.Unmarshal(dataJSON, &data)
+				Expect(err).Should(BeNil())
+				Expect(data).To(Equal(expectedResponseData))
+			})
+		})
+
+		Context("fail", func() {
+			It("should return invalid parameters", func() {
+				prefixURL := fmt.Sprintf("/api/%s/devices/tou/energy-cost", fixtures.UtGateway.UUID)
+				seedUtInvalidParamsURL := fmt.Sprintf("%s?startTime=%s&endTime=%s", prefixURL, "xxx", UtEndTime)
+				tt := testutils.TestInfo{
+					Token:      token,
+					URL:        seedUtInvalidParamsURL,
+					WantStatus: http.StatusBadRequest,
+					WantRv: app.Response{
+						Code: e.InvalidParams,
+						Msg:  "invalid parameters",
+					},
+				}
+				testutils.GinkgoAssertRequest(tt, router, "GET", nil)
+			})
+		})
+	})
+})
--- backend/scripts/der-ems-cc-data-log-calculated-daily.sql
+++ backend/scripts/der-ems-cc-data-log-calculated-daily.sql
@@ -1,4 +1,4 @@
-INSERT INTO der_ems.cc_data_log_calculated_daily (gw_uuid,latest_log_date,pv_produced_lifetime_energy_ac_diff,load_consumed_lifetime_energy_ac_diff,battery_lifetime_energy_ac_diff,grid_lifetime_energy_ac_diff,load_self_consumed_energy_percent_ac,created_at,updated_at) VALUES
-	 ('0E0BA27A8175AF978C49396BDE9D7A1E','2022-07-31 15:59:00',5,10,15,5,10,'2022-07-31 16:15:00','2022-07-31 16:15:00'),
-	 ('0E0BA27A8175AF978C49396BDE9D7A1E','2022-08-01 15:59:15',10,20,30,15,15,'2022-08-01 16:15:00','2022-08-01 16:15:00'),
-	 ('0E0BA27A8175AF978C49396BDE9D7A1E','2022-08-02 15:59:30',15,30,45,25,20,'2022-08-02 16:15:00','2022-08-02 16:15:00');
+INSERT INTO der_ems.cc_data_log_calculated_daily (gw_uuid,latest_log_date,pv_produced_lifetime_energy_ac_diff,load_consumed_lifetime_energy_ac_diff,battery_lifetime_energy_ac_diff,grid_lifetime_energy_ac_diff,load_self_consumed_energy_percent_ac,off_peak_period_pre_ubiik_cost,off_peak_period_post_ubiik_cost,on_peak_period_pre_ubiik_cost,on_peak_period_post_ubiik_cost,mid_peak_period_pre_ubiik_cost,mid_peak_period_post_ubiik_cost,created_at,updated_at) VALUES
+	 ('0E0BA27A8175AF978C49396BDE9D7A1E','2022-07-31 15:59:00',5,10,15,5,10,302.03,265.48,0,0,0,0,'2022-07-31 16:15:00','2022-07-31 16:15:00'),
+	 ('0E0BA27A8175AF978C49396BDE9D7A1E','2022-08-01 15:59:15',10,20,30,15,15,114.84,114.81,296.6,226.11,0,0,'2022-08-01 16:15:00','2022-08-01 16:15:00'),
+	 ('0E0BA27A8175AF978C49396BDE9D7A1E','2022-08-02 15:59:30',15,30,45,25,20,87.43,87.4,299.95,209.36,0,0,'2022-08-02 16:15:00','2022-08-02 16:15:00');

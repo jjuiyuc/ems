fb96301ba960778dc0f0b1dd1b9c3e310204fbb9
jjuiyuc <action5392@gmail.com>
2022-12-12 15:36:08 +0800

    style : formatting the code about unit

--- frontend/src/components/PriceCard.jsx
+++ frontend/src/components/PriceCard.jsx
@@ -1,5 +1,5 @@
 const PriceCard = props => <div className="card font-bold mb-8">
     <p className="mb-4">{props.title}</p>
-    <h2>$ {props.price}</h2>
+    <h2>${props.price}</h2>
 </div>
 export default PriceCard

d3c92827a9fdfec8b84bc57271dae7993c73308c
jjuiyuc <action5392@gmail.com>
2023-01-05 18:24:31 +0800

    feat: battery staatus animation

--- frontend/src/components/BatteryDiagram.jsx
+++ frontend/src/components/BatteryDiagram.jsx
@@ -1,31 +1,33 @@
 import styled from "styled-components"
-import { useState } from "react"
 
-const stopKeyframe = (level) => `
-  ${(1 - 1 / (level + 1)) * 100}% {
-    height: ${level * 10}%;
-  }`
-
+const stopKeyframe = (level, direction) => {
+    if (level == 100 && direction == "dischargingTo") return ""
+    const percentage = direction == "dischargingTo" ? (100 - level) : level
+    return `${percentage}% {
+      height: ${level}%;
+    }`
+}
 const Animation = styled.div`
   @keyframes animation {
     from {
-      height: 0%
+      height: 0%;
     }
-    ${(props) => stopKeyframe(props.level)}
+    ${(props) => stopKeyframe(props.level, props.direction)}
     to {
-      height: ${(props) => props.level * 10 + "%"}
+      height: ${(props) => (props.level) + "%"};
     }
   }
-  animation-duration: ${(props) => props.level + 1 + "s"}
-  animation-iteration-count: infinite
-  animation-name: animation;
-  animation-timing-function: linear`
+  animation-duration: ${(props) => (props.level / 10 * 1.1) + "s"};
+  animation-iteration-count: infinite;
+  animation-name: ${(props) => props.level == 100 && props.direction == "chargingFrom"
+        ? "" : "animation"};
+  animation-timing-function: linear;`
 
-export default function BatteryDiagram() {
-
-    const [batteryLevel] = useState(6),
-        [direction] = useState("charge")
-
+export default function BatteryDiagram(props) {
+    const { direction } = props
+    const state = props.state > 100
+        ? 100
+        : (props.state < 0 ? 0 : props.state)
     const lines = Array.from(Array(10).keys()).map((key) => (
         <div
             className={"border-white" + (key < 9 ? " border-b-2" : "")}
@@ -33,27 +35,36 @@ export default function BatteryDiagram() {
         />
     ))
     return (
-        <div className="p-2">
-            <div className="flex flex-col items-center w-24">
-                <div className="bg-gray-400 h-3 w-10 rounded-t-md" />
-                <div className="bg-gray-400 h-48 p-2 rounded-lg w-full">
-                    <div className="bg-white h-full p-1 rounded-md">
-                        <div className="h-full overflow-hidden relative rounded">
-                            <Animation
-                                className={
-                                    "absolute bg-green-500 w-full " +
-                                    (direction === "charge" ? "bottom-0" : "")
-                                }
-                                level={batteryLevel}
-                                style={{
-                                    top:
-                                        direction === "discharge"
-                                            ? 100 - batteryLevel * 10 + "%"
-                                            : ""
-                                }}
-                            />
-                            <div className="absolute grid h-full w-full">{lines}</div>
-                        </div>
+        <div className="flex flex-col items-center w-24">
+            <div className="bg-gray-400 h-3 w-10 rounded-t-md" />
+            <div className="bg-gray-400 h-48 p-2 rounded-lg w-full">
+                <div className="bg-white h-full p-1 rounded-md">
+                    <div className="h-full overflow-hidden relative rounded">
+                        <div className={"absolute bg-primary-200 w-full " +
+                            (direction === "chargingFrom" ? "bottom-0" : "")}
+                            style={{
+                                height: (state || 0) + "%",
+                                top:
+                                    direction === "dischargingTo"
+                                        ? (100 - (state || 0)) + "%"
+                                        : ""
+                            }}></div>
+                        <Animation
+                            className={
+                                "absolute bg-primary-500 w-full " +
+                                (direction === "chargingFrom" ? "bottom-0" : "")
+                            }
+                            direction={direction}
+                            level={(state || 0)}
+                            style={{
+                                height: (state || 0) + "%",
+                                top:
+                                    direction === "dischargingTo"
+                                        ? (100 - (state || 0)) + "%"
+                                        : ""
+                            }}
+                        />
+                        <div className="absolute grid h-full w-full">{lines}</div>
                     </div>
                 </div>
             </div>

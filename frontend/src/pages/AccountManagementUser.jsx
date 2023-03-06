3ac881d3324a35d7d18aade109c913b0a276e866
jjuiyuc <action5392@gmail.com>
2023-03-06 17:58:24 +0800

    feat: update h1 in AccountManagementGroup, AccountManagementUser page

--- frontend/src/pages/AccountManagementUser.jsx
+++ frontend/src/pages/AccountManagementUser.jsx
@@ -1,6 +1,15 @@
-const AccountManagementUser = props => {
+import DataTable from "react-data-table-component"
+import moment from "moment"
+import { useTranslation } from "react-multi-lang"
+import { useEffect, useMemo, useState } from "react"
 
+export default function AccountManagementUser() {
+    const
+        t = useTranslation(),
+        commonT = string => t("common." + string),
+        pageT = (string, params) => t("accountManagementUser." + string, params)
     return <>
+        <h1 className="mb-9">{commonT("accountManagementUser")}</h1>
     </>
+
 }
-export default AccountManagementUser

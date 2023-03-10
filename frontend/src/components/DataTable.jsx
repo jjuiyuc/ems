dcfa2570a120fea4e2a05c3e3c77f3ce5bde771b
jjuiyuc <action5392@gmail.com>
2023-03-10 11:40:28 +0800

    feat: DataTable component

---
+++ frontend/src/components/DataTable.jsx
@@ -0,0 +1,16 @@
+import DataTable from "react-data-table-component"
+import { useTranslation } from "react-multi-lang"
+
+import DATATABLE_STYLES from "../configs/dataTableStyles"
+
+export default function Table(props) {
+    const t = useTranslation(),
+        tableT = string => t("dataTable." + string)
+
+    return <DataTable
+        {...props}
+        customStyles={DATATABLE_STYLES}
+        noDataComponent={tableT("noDataMsg")}
+        pagination={true}
+        paginationRowsPerPageOptions={[10, 25, 50, 100]} />
+}

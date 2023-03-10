51a87f61684c080662ae74c5c5432c4e12c8e01f
jjuiyuc <action5392@gmail.com>
2023-03-10 11:42:51 +0800

    feat: UI/ AccountManagementUser, AccountManagementGroup (main page)

--- frontend/src/pages/AccountManagementGroup.jsx
+++ frontend/src/pages/AccountManagementGroup.jsx
@@ -5,26 +5,92 @@ import moment from "moment"
 import { useTranslation } from "react-multi-lang"
 import { useEffect, useMemo, useState } from "react"
 
+import Table from "../components/DataTable"
+
+import { ReactComponent as DeleteIcon } from "../assets/icons/trash_solid.svg"
+import { ReactComponent as EditIcon } from "../assets/icons/edit.svg"
+import { ReactComponent as NoticeIcon } from "../assets/icons/notice.svg"
+
 export default function AccountManagementGroup() {
     const
         t = useTranslation(),
         commonT = string => t("common." + string),
         pageT = (string, params) => t("accountManagementGroup." + string, params)
+    const
+        [data, setData] = useState([
+            {
+                id: 1,
+                groupName: "AreaOwner_TW",
+                groupType: "Area Owner"
+            },
+            {
+                id: 2,
+                groupName: "AreaMaintainer_TW",
+                groupType: "Area Maintainer"
+            },
+            {
+                id: 3,
+                groupName: "Serenegray",
+                groupType: "Field Owner"
+            },
+            {
+                id: 4,
+                groupName: "Cht_Miaoli",
+                groupType: "Area maintainer"
+            }
+        ]),
+        [error, setError] = useState(null),
+        [loading, setLoading] = useState(false)
+
+    const columns = [
+        {
+            cell: row => <span className="font-mono">{row.groupName}</span>,
+            center: true,
+            name: pageT("groupName"),
+            selector: row => row.groupName
+        },
+        {
+            cell: row => <span className="font-mono">{row.groupType}</span>,
+            center: true,
+            name: pageT("groupType"),
+            selector: row => row.groupType
+        },
+        {
+            cell: row => <div className="flex w-28">
+                <NoticeIcon className="mr-5" />
+                {row.groupType === "Area Owner"
+                    ? null
+                    : <>
+                        <EditIcon className="mr-5" />
+                        <DeleteIcon />
+                    </>}
+            </div>,
+            center: true,
+        }
+    ]
     return <>
         <h1 className="mb-9">{commonT("accountManagementGroup")}</h1>
-        <div>
+        <div className="mb-9">
             <Button
                 // onClick={}
                 key={"ac-b-"}
-                size="large"
+                size="x-large"
                 variant="outlined"
+                radius="pill"
+                fontSize="large"
                 color="brand"
                 startIcon={<AddIcon />}>
                 {commonT("add")}
             </Button>
         </div>
-
+        <Table
+            {...{ columns, data }}
+            paginationComponentOptions={{
+                rowsPerPageText: t("dataTable.rowsPerPage")
+            }}
+            paginationPerPage={100}
+            progressPending={loading}
+            theme="dark"
+        />
     </>
-
-
 }
--- frontend/src/pages/AccountManagementUser.jsx
+++ frontend/src/pages/AccountManagementUser.jsx
@@ -5,24 +5,111 @@ import moment from "moment"
 import { useTranslation } from "react-multi-lang"
 import { useEffect, useMemo, useState } from "react"
 
+import Table from "../components/DataTable"
+
+import { ReactComponent as DeleteIcon } from "../assets/icons/trash_solid.svg"
+import { ReactComponent as EditIcon } from "../assets/icons/edit.svg"
+
 export default function AccountManagementUser() {
     const
         t = useTranslation(),
         commonT = string => t("common." + string),
         pageT = (string, params) => t("accountManagementUser." + string, params)
+    const
+        [data, setData] = useState([
+            {
+                id: 1,
+                account: "XXXXX@ubiik.com",
+                name: "XXXXX",
+                group: "Area Owner_TW"
+            },
+            {
+                id: 2,
+                account: "YYYYY@ubiik.com",
+                name: "YYYYY",
+                group: "AreaMaintainer_TW"
+            },
+            {
+                id: 3,
+                account: "serenegray@ubiik.com",
+                name: "Serenegray",
+                group: "Serenegray"
+            },
+            {
+                id: 4,
+                account: "cht_miaoli@ubiik.com",
+                name: "Cht_Miaoli",
+                group: "Cht_Miaoli"
+            }
+        ]),
+        [error, setError] = useState(null),
+        [loading, setLoading] = useState(false)
+
+    const columns = [
+        {
+            cell: row => <span className="font-mono">{row.account}</span>,
+            center: true,
+            name: pageT("account"),
+            selector: row => row.account
+        },
+        {
+            cell: row => <span className="font-mono">{row.name}</span>,
+            center: true,
+            name: pageT("name"),
+            selector: row => row.name
+        },
+        {
+            cell: row => <span className="font-mono">{row.group}</span>,
+            center: true,
+            name: commonT("group"),
+            selector: row => row.group
+        },
+        {
+            cell: row => <div className="flex w-28">
+                <EditIcon className="mr-5" />
+                {row.group === "Area Owner_TW"
+                    ? null
+                    : <DeleteIcon />
+                }
+            </div>,
+            center: true,
+        }
+    ]
     return <>
         <h1 className="mb-9">{commonT("accountManagementUser")}</h1>
-        <div>
+        <div className="mb-9">
             <Button
                 // onClick={}
                 key={"ac-b-"}
-                size="large"
+                size="x-large"
                 variant="outlined"
+                radius="pill"
                 color="brand"
                 startIcon={<AddIcon />}>
                 {commonT("add")}
             </Button>
         </div>
+        <Table
+            {...{ columns, data }}
+            customStyles={{
+                headRow: {
+                    style: {
+                        backgroundColor: "#12c9c990",
+                        fontWeight: 600,
+                        fontSize: "16px",
+                        borderRadius: ".45rem .45rem 0 0 "
+                    }
+                }
+            }}
+            noDataComponent={t("dataTable.noDataMsg")}
+            pagination={true}
+            paginationComponentOptions={{
+                rowsPerPageText: t("dataTable.rowsPerPage")
+            }}
+            paginationPerPage={100}
+            progressPending={loading}
+            theme="dark"
+        />
     </>
 
 }

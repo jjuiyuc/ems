c06b8e45037b9697220396b236700bb705847007
jjuiyuc <action5392@gmail.com>
2023-03-27 17:39:45 +0800

    feat: update text field from "deviceType" to "chargingSource"

--- frontend/src/components/AddField.jsx
+++ frontend/src/components/AddField.jsx
@@ -1,17 +1,21 @@
 import {
-    Button, DialogActions, Divider, FormControl, ListItem,
+    Button, DialogActions, Divider, FormControl, InputAdornment, ListItem,
     MenuItem, TextField
 } from "@mui/material"
 import AddIcon from "@mui/icons-material/Add"
 import { useTranslation } from "react-multi-lang"
 import { useEffect, useMemo, useState } from "react"
+import { ValidateNumPercent } from "../utils/utils"
 
 import DialogForm from "../components/DialogForm"
 
 export default function AddField({
     children = null,
     dialogTitle = "",
-    locationTitle,
+    locationInfo,
+    fieldDevices,
+    deviceInfo,
+    extraDeviceInfo,
     open,
     setOpen,
     closeOutside = false
@@ -39,9 +43,39 @@ export default function AddField({
                 value: "twoSection",
                 label: "twoSection",
             }
+        ],
+        deviceTypes = [
+            {
+                value: "hybridInverter",
+                label: "hybridInverter",
+            },
+            {
+                value: "inverter",
+                label: "inverter",
+            },
+            {
+                value: "meter",
+                label: "meter",
+            },
+            {
+                value: "pv",
+                label: "pv",
+            },
+            {
+                value: "battery",
+                label: "battery",
+            },
+            {
+                value: "pcs",
+                label: "pcs",
+            }
+        ],
+        deviceModel = [
+            {
+                value: "LXP-12K US-Luxpower Hybrid-Inverter",
+                label: "LXP-12K US-Luxpower Hybrid-Inverter"
+            }
         ]
-
-
     const
         t = useTranslation(),
         commonT = string => t("common." + string),
@@ -49,10 +83,28 @@ export default function AddField({
         formT = (string) => t("form." + string)
 
     const
+        [deviceType, setDeviceType] = useState(""),
+        [gridOutagePercent, setGridOutagePercent] = useState(""),
+        [chargingSource, setChargingSource] = useState([
+            {
+                value: "solar+grid",
+                label: "solar+grid",
+            },
+            {
+                value: "solar",
+                label: "solar",
+            }
+        ]),
         [fullWidth, setFullWidth] = useState(true),
-        [maxWidth, setMaxWidth] = useState("xl"),
+        [maxWidth, setMaxWidth] = useState("lg"),
         [openAdd, setOpenAdd] = useState(false)
-
+    const
+        inputPercent = (e) => {
+            const num = e.target.value
+            const isNum = ValidateNumPercent(num)
+            if (!isNum) return
+            setGridOutagePercent(num)
+        }
     return <>
         <Button
             onClick={() => { setOpenAdd(true) }}
@@ -80,39 +132,41 @@ export default function AddField({
                 minWidth: 120
             }}
                 fullWidth={true}>
-                <div className="flex-nowrap">
+                <div className="grid grid-cols-1fr-auto items-center mb-8">
                     <TextField
+                        sx={{ marginBottom: 0 }}
                         id="gatewayID"
                         label={commonT("gatewayID")}
                     // value={gatewayID}
-                    // focused
                     />
                     <Button
                         // onClick={}
-                        sx={{ margin: "0.5rem 0.5rem 0.5rem 1rem" }}
+                        sx={{ marginLeft: "0.3rem" }}
                         radius="pill"
                         variant="contained"
                         color="primary">
                         {commonT("verify")}
                     </Button>
                 </div>
-                <h5 className="mb-4 ml-2">{locationTitle}</h5>
+                <h5 className="mb-5 ml-2">{locationInfo}</h5>
                 <TextField
                     id="address"
+                    // value={address}
                     label={formT("address")}
-                // value={address}
                 />
                 <div className="flex-nowrap">
                     <TextField
                         id="lat"
+                        type="number"
                         label={formT("lat")}
                     // value={lat}
                     />
                     <TextField
                         id="lng"
+                        type="number"
                         label={formT("lng")}
+                        // value={lng}
                         sx={{ marginLeft: "1rem" }}
-                    // value={lng}
                     />
                 </div>
                 <TextField
@@ -151,7 +205,96 @@ export default function AddField({
                         </MenuItem>
                     ))}
                 </TextField>
+                <Divider variant="middle" />
+                <h5 className="mt-8 mb-5 ml-2">{fieldDevices}</h5>
+                <TextField
+                    id="deviceType"
+                    select
+                    label={formT("deviceType")}
+                    defaultValue=""
+                >
+                    {deviceTypes.map((option) => (
+                        <MenuItem
+                            key={option.value}
+                            value={formT(`${option.value}`)}
+                            onClick={() => {
+                                setDeviceType(option)
+                                console.log(deviceType)
+                            }}>
+                            {formT(`${option.label}`)}
+                        </MenuItem>
 
+                    ))}
+                </TextField>
+                <TextField
+                    id="deviceModel"
+                    select
+                    label={formT("deviceModel")}
+                    defaultValue=""
+                >
+                    {deviceModel.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <h5 className="mb-5 ml-2">{deviceInfo}</h5>
+                <TextField
+                    id="modbusID"
+                    type="number"
+                    label={formT("modbusID")}
+                // value={modbusID}
+                />
+                <div className="grid grid-cols-1fr-auto items-center mb-8">
+                    <TextField
+                        sx={{ marginBottom: 0 }}
+                        id="UUEID"
+                        label="UUEID"
+                    // value={UUEID}
+                    />
+                    <Button
+                        // onClick={}
+                        sx={{ marginLeft: "0.3rem" }}
+                        radius="pill"
+                        variant="contained"
+                        color="primary">
+                        {commonT("verify")}
+                    </Button>
+                </div>
+                <TextField
+                    id="powerCapacity"
+                    type="number"
+                    label={formT("powerCapacity")}
+                // value={powerCapacity}
+                />
+                <Divider variant="middle" />
+                {deviceType.value === "battery"
+                    ? <>
+                        <h5 className="mt-8 mb-5 ml-2">{extraDeviceInfo}</h5>
+                        <TextField
+                            id="reservedForGridOutagePercent"
+                            label={formT("reservedForGridOutagePercent")}
+                            value={gridOutagePercent}
+                            onChange={inputPercent}
+                            InputProps={{
+                                endAdornment:
+                                    <InputAdornment position="end">%</InputAdornment>
+                            }}
+                        />
+                        <TextField
+                            id="chargingSource"
+                            select
+                            label={formT("chargingSource")}
+                            defaultValue=""
+                        >
+                            {chargingSource.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </>
+                    : null}
             </FormControl>
             <Divider variant="middle" />
             <DialogActions sx={{ margin: "1rem 0.5rem 1rem 0" }}>

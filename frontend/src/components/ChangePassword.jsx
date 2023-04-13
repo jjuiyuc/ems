4dc86f5cff20a81171b5139b7ada83457e20a0c6
jjuiyuc <action5392@gmail.com>
2023-04-13 17:48:09 +0800

    feat: destruct component "ChangePassword" from Account page

---
+++ frontend/src/components/ChangePassword.jsx
@@ -0,0 +1,132 @@
+import { connect } from "react-redux"
+import { Button, Divider, IconButton, InputAdornment, OutlinedInput } from "@mui/material"
+import Visibility from "@mui/icons-material/Visibility"
+import VisibilityOff from "@mui/icons-material/VisibilityOff"
+import { useTranslation } from "react-multi-lang"
+import { useEffect, useState } from "react"
+
+import { apiCall } from "../utils/api"
+import { ValidatePassword } from "../utils/utils"
+
+
+export default function ChangePassword(props) {
+    const
+        t = useTranslation(),
+        commonT = string => t("common." + string),
+        formT = (string) => t("form." + string),
+        pageT = (string, params) => t("account." + string, params),
+        errorT = (string) => t("error." + string)
+
+    const
+        [curPassword, setCurPassword] = useState(""),
+        [curPasswordError, setCurPasswordError] = useState(false),
+        [newPassword, setNewPassword] = useState(""),
+        [newPasswordError, setNewPasswordError] = useState(false),
+        [showPassword, setShowPassword] = useState(false),
+        [loading, setLoading] = useState(false)
+
+    const
+        handleClickShowPassword = () => setShowPassword((show) => !show),
+        handleMouseDownPassword = (event) => {
+            event.preventDefault()
+        },
+        changePassword = (e) => {
+            setCurPassword(e.target.value)
+            setCurPasswordError(false)
+            // setOtherError("")
+        },
+        changeNewPassword = (e) => {
+            setNewPassword(e.target.value)
+            setNewPasswordError(false)
+            // setOtherError("")
+        },
+        submit = () => {
+            apiCall({
+                data: {
+                    currentPassword: curPassword,
+                    newPassword: newPassword
+                },
+                method: "put",
+                onSuccess: () => console.log("okk"),
+                url: "/api/users/password"
+            })
+        }
+
+    return <>
+        <div className="card w-fit">
+            <h4 className="mb-6">
+                {pageT("changePassword")}
+            </h4>
+            <Divider variant="fullWidth" sx={{ marginBottom: "1.5rem" }} />
+            <form className="grid grid-cols-1fr-auto gap-x-5 gap-y-6">
+                <label className="pt-2">{pageT("currentPassword")}</label>
+                <OutlinedInput
+                    id="cur-password"
+                    size="small"
+                    type={showPassword ? "text" : "password"}
+                    value={curPassword || ""}
+                    onChange={changePassword}
+                    autoComplete="current-password"
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {showPassword
+                                    ? <Visibility />
+                                    : <VisibilityOff />
+                                }
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                />
+                <label className="pt-2">{pageT("newPassword")}</label>
+                <OutlinedInput
+                    id="new-password"
+                    size="small"
+                    type={showPassword ? "text" : "password"}
+                    value={newPassword || ""}
+                    onChange={changeNewPassword}
+                    autoComplete="current-password"
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {showPassword
+                                    ? <Visibility />
+                                    : <VisibilityOff />
+                                }
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                />
+
+            </form>
+            <Divider variant="fullWidth" sx={{ marginTop: "1.5rem" }} />
+            <div className="flex flex-row-reverse mt-6">
+                <Button
+                    sx={{ marginLeft: "0.5rem" }}
+                    onClick={submit}
+                    radius="pill"
+                    variant="contained"
+                    color="primary">
+                    {commonT("save")}
+                </Button>
+                <Button
+                    // onClick={}
+                    variant="outlined"
+                    radius="pill"
+                    color="gray">
+                    {commonT("cancel")}
+                </Button>
+            </div>
+        </div>
+    </>
+}
--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,13 +1,7 @@
-import { Button, Divider, InputAdornment, IconButton, OutlinedInput } from "@mui/material"
-import Visibility from "@mui/icons-material/Visibility"
-import VisibilityOff from "@mui/icons-material/VisibilityOff"
 import { useTranslation } from "react-multi-lang"
-import { useState } from "react"
-
-import { ValidatePassword } from "../utils/utils"
-import { apiCall } from "../utils/api"
 
 import AccountInfoModify from "../components/AccountInfoModify"
+import ChangePassword from "../components/ChangePassword"
 
 export default function Account() {
 
@@ -17,130 +11,13 @@ export default function Account() {
         pageT = (string, params) => t("account." + string, params),
         errorT = (string) => t("error." + string)
 
-    const
-        [data, setData] = useState(
-            {
-                id: 3,
-                account: "serenegray@ubiik.com",
-                password: "9977xxxxll",
-                name: "Serenegray"
-            }
-        ),
-        [curPassword, setCurPassword] = useState(""),
-        [curPasswordError, setCurPasswordError] = useState(false),
-        [newPassword, setNewPassword] = useState(""),
-        [newPasswordError, setNewPasswordError] = useState(false),
-        [showPassword, setShowPassword] = useState(false),
-        [loading, setLoading] = useState(false)
-
-    const
-        handleClickOpen = () => {
-            setOpen(true)
-        },
-        handleClose = () => {
-            setOpen(false)
-        },
-        handleClickShowPassword = () => setShowPassword((show) => !show),
-        handleMouseDownPassword = (event) => {
-            event.preventDefault()
-        },
-        handleChange = (e) => {
-            setDefaultValue(e.target.value)
-        }
-    const
-        changePassword = (e) => {
-            setCurPassword(e.target.value)
-            setCurPasswordError(false)
-            setOtherError("")
-        },
-        changeNewPassword = (e) => {
-            setNewPassword(e.target.value)
-            setNewPasswordError(false)
-            setOtherError("")
-        }
 
     return <>
         <h1 className="mb-9">{commonT("account")}</h1>
         {/* <div className="card flex flex-col m-auto mt-4 min-w-49 w-fit"> */}
         <div className="gap-y-5 flex flex-wrap lg:gap-x-5">
             <AccountInfoModify />
-            <div className="card w-fit">
-                <h4 className="mb-6">
-                    {pageT("changePassword")}
-                </h4>
-                <Divider variant="fullWidth" sx={{ marginBottom: "1.5rem" }} />
-                <form className="grid grid-cols-1fr-auto gap-x-5 gap-y-6">
-                    <label className="pt-2">{pageT("currentPassword")}</label>
-                    <OutlinedInput
-                        id="cur-password"
-                        size="small"
-                        type={showPassword ? "text" : "password"}
-                        value={curPassword || ""}
-                        onChange={changePassword}
-                        autoComplete="current-password"
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                >
-                                    {showPassword
-                                        ? <Visibility />
-                                        : <VisibilityOff />
-                                    }
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                    <label className="pt-2">{pageT("newPassword")}</label>
-                    <OutlinedInput
-                        id="new-password"
-                        size="small"
-                        type={showPassword ? "text" : "password"}
-                        value={newPassword || ""}
-                        onChange={changeNewPassword}
-                        autoComplete="current-password"
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                >
-                                    {showPassword
-                                        ? <Visibility />
-                                        : <VisibilityOff />
-                                    }
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-
-                </form>
-                <Divider variant="fullWidth" sx={{ marginTop: "1.5rem" }} />
-                <div className="flex flex-row-reverse mt-6">
-                    <Button
-                        sx={{ marginLeft: "0.5rem" }}
-                        // onClick={() => {
-
-                        // }}
-                        radius="pill"
-                        variant="contained"
-                        color="primary">
-                        {commonT("save")}
-                    </Button>
-                    <Button
-                        // onClick={}
-                        variant="outlined"
-                        radius="pill"
-                        color="gray">
-                        {commonT("cancel")}
-                    </Button>
-                </div>
-            </div>
+            <ChangePassword />
         </div>
     </>
 }

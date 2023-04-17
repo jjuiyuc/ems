5e666884197951798524f8233f4cb476bb012324
jjuiyuc <action5392@gmail.com>
2023-04-17 17:53:09 +0800

    feat: update store, user.js + add snackbarMsgSlice.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,9 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 
 import gateways from "./reducers/gateways"
 import lang from "./reducers/lang"
 import sidebarStatus from "./reducers/sidebarStatus"
+import snackbarMsg from "./reducers/snackbarMsg"
 import user from "./reducers/user"
 
-export default configureStore({reducer: {gateways, lang, sidebarStatus, user}})
+export default configureStore({ reducer: { gateways, lang, sidebarStatus, snackbarMsg, user } })

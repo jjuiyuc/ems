cdb1dda16fd01185c0c9ce558ba619609ebc08d2
jjuiyuc <action5392@gmail.com>
2023-04-18 17:37:11 +0800

    style: remove unnecessary code

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -6,16 +6,12 @@ import ChangePassword from "../components/ChangePassword"
 export default function Account() {
 
     const t = useTranslation(),
-        commonT = string => t("common." + string),
-        dialogT = (string) => t("dialog." + string),
-        pageT = (string, params) => t("account." + string, params),
-        errorT = (string) => t("error." + string)
-
+        commonT = string => t("common." + string)
 
     return <>
         <h1 className="mb-9">{commonT("account")}</h1>
         {/* <div className="card flex flex-col m-auto mt-4 min-w-49 w-fit"> */}
-        <div className="gap-y-5 flex flex-wrap lg:gap-x-5">
+        <div className="gap-y-5 flex flex-wrap md:gap-x-5">
             <AccountInfoModify />
             <ChangePassword />
         </div>

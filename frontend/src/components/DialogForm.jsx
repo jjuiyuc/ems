cd6cc5baee0c1011941ff30e67ac1f2158cbd8eb
jjuiyuc <action5392@gmail.com>
2023-06-30 15:28:31 +0800

    style: update CSS about dialog box-shadow

--- frontend/src/components/DialogForm.jsx
+++ frontend/src/components/DialogForm.jsx
@@ -8,8 +8,8 @@ const CustomDialog = styled(Dialog)(({ theme }) => ({
         color: theme.palette.gray[200],
         backgroundColor: theme.palette.gray[900],
         backgroundImage: "none",
-        border: "1px solid" + theme.palette.gray[400],
-        boxShadow: "0px 2px 6px 2px rgba(0,0,0,0.14), 0px 2px 6px 2px rgba(96,96,96,0.4)"
+        border: "2px solid" + theme.palette.gray[400],
+        boxShadow: "0px 2px 6px 2px rgba(0,0,0,0.14), 0px 2px 8px 2px rgba(96,96,96,0.4)"
     },
 }))
 

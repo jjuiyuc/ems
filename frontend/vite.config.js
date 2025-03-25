f476f20385a52b31c81b9b24c3a6b5fccc36734c
jjuiyuc <action5392@gmail.com>
2025-03-25 16:33:36 +0800

    feat: set base path for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import {defineConfig} from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    define: {
-        "global": {},
-        "process.env": process.env
-    },
-    plugins: [react(), svgr()]
+    base: "/ems/",
+    plugins: [react(), svgr()],
 })
+// export default defineConfig({
+//     define: {
+//         "global": {},
+//         "process.env": process.env
+//     },
+//     plugins: [react(), svgr()]
+// })
